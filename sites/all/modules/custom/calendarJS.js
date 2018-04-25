04d418456d6a6c6003209b4422ba4bab29b6f14f
Sagun Maharjan <sagun_90@hotmail.com>
2018-04-25 06:50:59 +0000

    Final commit

--- sites/all/modules/custom/calendarJS.js
+++ sites/all/modules/custom/calendarJS.js
@@ -1,22 +1,23 @@
 window.onload = function() {
     
-    let colors = ["#B22222", "Blue", "#1F5F7A", "#556B2F", "Red", "#CD5C5C", "Brown", "Black","#BDB76B", "#696969", "DarkGoldenRod", "DarkOrchid","DarkOrange", "#CD853F", "Green"];
+    let colors = ["#B22222", "Blue", "#1F5F7A", "#556B2F", "Red", "#CD5C5C", "DeepPink", "Black","#BDB76B", "#696969", "DarkGoldenRod", "DarkOrchid","DarkOrange", "#CD853F", "Green", "#FFC300", "#FF6E6E", "#766738", "#387659", "#9800C0"];
    // let colors = ["DeepPink", "Blue", "Chartreuse", "Red", "Aqua", "Brown", "Aquamarine", "Black", "Cyan", "DarkGoldenRod", "DarkOrchid","DarkOrange", "Gold", "Green", "GreenYellow"];
-    let courses = ['NURS2004', 'NURS2009', 'NURS2011', 'NURS2013', 'NURS3009', 'NURS3010', 'NURS3011', 'NURS3028', 'NURS3029', 'NURS3030', 'NURS4000', 'NURS4001', 'NURS4002', 'NURS4066', 'NURS4067'];
+    let courses = ['NURS2004', 'NURS2009', 'NURS2011', 'NURS2013', 'NURS3009', 'NURS3010', 'NURS3011', 'NURS3028', 'NURS3029', 'NURS3030', 'NURS4000', 'NURS4001', 'NURS4002', 'NURS4066', 'NURS4067', 'NURS2000', 'NURS2002', 'NURS5005', 'NURS5226', 'NURS2003'];
 
-    let x = document.getElementsByClassName("views-field views-field-field-course-names");
     let eventBorder = document.getElementsByClassName("calendar monthview");
     let output;
-    for (output = 0; output < x.length; output++) {
-        x[output].style.color = "blue";
-        var htmlString = x[output].innerHTML;
-        var courseName = htmlString.substring(htmlString.lastIndexOf("content\">")+9,htmlString.lastIndexOf("<"));
+    for (output = 0; output < eventBorder.length; output++) {
+        if(eventBorder[output].querySelectorAll("span.field-content")[0]) {
+        var courseName = eventBorder[output].querySelectorAll("span.field-content")[0].getElementsByTagName("a")[0].innerHTML;
         if (checkValue(courseName, courses) === 'Exist')
         {
-            x[output].style.color = colors[courses.indexOf(courseName)];
-            eventBorder[output].style.borderLeft = "4px solid " + colors[courses.indexOf(courseName)];
+            
+            eventBorder[output].querySelectorAll("span.field-content")[0].getElementsByTagName("a")[0].setAttribute("style", "color: " + colors[courses.indexOf(courseName.substr(0, 8))] + " !important");
+            eventBorder[output].style.borderLeft = "4px solid " + colors[courses.indexOf(courseName.substr(0, 8))];
 
         }
+    }
+    
     }
 };
 function checkValue(value,arr){
@@ -24,7 +25,7 @@ function checkValue(value,arr){
 
     for(var i=0; i<arr.length; i++){
         var name = arr[i];
-        if(name === value){
+        if(value.indexOf(name) > -1){
             status = 'Exist';
             break;
         }
--- sites/all/modules/custom/newCalendarJS.js
+++ sites/all/modules/custom/newCalendarJS.js
@@ -1,22 +1,54 @@
-window.onload = function() {
+window.onload = function() 
+{
+
+    let courseColors = ["#B22222", "Blue", "#1F5F7A", "#556B2F", "Red", "#CD5C5C", "Brown", "Black","#BDB76B", "#696969", "DarkGoldenRod", "DarkOrchid","DarkOrange", "#CD853F", "Green", "#FFC300", "#FF6E6E", "#766738", "#387659", "#9800C0"];
+    let courseNames = ['NURS2004', 'NURS2009', 'NURS2011', 'NURS2013', 'NURS3009', 'NURS3010', 'NURS3011', 'NURS3028', 'NURS3029', 'NURS3030', 'NURS4000', 'NURS4001', 'NURS4002', 'NURS4066', 'NURS4067', 'NURS2000', 'NURS2002', 'NURS5005', 'NURS5226', 'NURS2003'];
+
+    //Select all <span class="field-content"> tags
+    let initialTag = document.querySelectorAll("span.field-content");
     
-    let colors = ["#B22222", "Blue", "#1F5F7A", "#556B2F", "Red", "#CD5C5C", "Brown", "Black","#BDB76B", "#696969", "DarkGoldenRod", "DarkOrchid","DarkOrange", "#CD853F", "Green"];
-   // let colors = ["DeepPink", "Blue", "Chartreuse", "Red", "Aqua", "Brown", "Aquamarine", "Black", "Cyan", "DarkGoldenRod", "DarkOrchid","DarkOrange", "Gold", "Green", "GreenYellow"];
-    let courses = ['NURS2004', 'NURS2009', 'NURS2011', 'NURS2013', 'NURS3009', 'NURS3010', 'NURS3011', 'NURS3028', 'NURS3029', 'NURS3030', 'NURS4000', 'NURS4001', 'NURS4002', 'NURS4066', 'NURS4067'];
+    //Selects every event on the calendar with the class name "calendar monthview"
+    let borderHTMLClass = document.getElementsByClassName("calendar monthview");
+    //counts the actual number of events that were given a color
+    let eventCount = 0;
 
-    let x = document.getElementsByClassName("field-content");
-    //let eventBorder = document.getElementsByClassName("calendar monthview");
-    for (let output = 0; output < x.length; output++) {
-        var htmlCourses = x[output].getElementsByTagName("a");
-        var courseName = htmlCourses[output].innerHTML;
-        if (checkValue(courseName, courses) === 'Exist')
+    //For each <span class="field-content"> tag select the first <a> tag
+    for (let count = 0; count < initialTag.length; count++) 
+    {
+        //Select the all <a> tags inside of <span class="field-content">
+        var aTag = initialTag[count].getElementsByTagName("a");
+        //Copy the innerHTML of the first <a> tag since it should contain the course name
+        var htmlCourseName = aTag[0].innerHTML;
+
+        //Apply the designated color to the <a> tag thus changing the color of the course name on the calendar
+        //First check if the htmlCourseName exists in the preset courseNames array
+        if (checkValue(htmlCourseName, courseNames) === 'Exist')
         {
-            htmlCourses[output].setAttribute("style", "color: " + colors[courses.indexOf(courseName)] + " !important");
-            //eventBorder[output].style.borderLeft = "4px solid " + colors[courses.indexOf(courseName)];
+            aTag[0].setAttribute("style", "color: " + courseColors[courseNames.indexOf(htmlCourseName)] + " !important");
+            
+            let aTagChecker = borderHTMLClass[eventCount].getElementsByTagName("a");
+            for(let i = 0; i < borderHTMLClass.length; i++)
+                if(borderHTMLClass[i].querySelectorAll("span.field-content")[0])
+                    console.log(borderHTMLClass[i].querySelectorAll("span.field-content")[0].getElementsByTagName("a")[0].innerHTML);
+            if (checkValue(aTagChecker[0].innerHTML, courseNames) === 'Exist')
+            
+            {
 
+                borderHTMLClass[eventCount].style.borderLeft = "4px solid " + courseColors[courseNames.indexOf(htmlCourseName)];
+               // aTag[eventCount].style.borderLeft = "4px solid " + courseColors[courseNames.indexOf(htmlCourseName)];
+                
+                
+            } 
+            eventCount++;
+            
         }
     }
+
 };
+
+    
+//Checks if a value is in an array
+//If there is a value it returns 'Exists'
 function checkValue(value,arr){
     var status = 'Not exist';
 
